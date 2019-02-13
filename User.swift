60b95060afd8596a399362028ed03b2653af9c79
sisayur <sisa@ua.fm>
2019-02-13 17:22:58 +0200

    Add User.swift

---
+++ User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  UserID
+//
+//  Created by Creo Pad on 5/31/17.
+//  Copyright © 2017 Iurii Popov. All rights reserved.
+//
+
+
+class User {
+
+    var name: String = ""
+    var age: Int = 0
+
+}
